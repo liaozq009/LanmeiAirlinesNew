605ce6e5a018727351512d7254e14f8f9632d9bb
liaozq009 <245822113@qq.com>
2018-03-07 18:03:28 +0800

    首页动画

--- js/EN/LanmeiAirlines.js
+++ js/EN/LanmeiAirlines.js
@@ -8,7 +8,74 @@ var LanmeiAirlines = {
 
 	/* 机票选择 */
 	ticketSelect:function(){
-		// 日期选择--动态加载
+		/* 出发地和目的地选择 */
+		var $mask = $('.js-flight-mask'); //遮罩层
+		var $box = $('.js-popup-box'); //c3动画最外层
+		var $content = $('.js-popup-content'); //c3动画内容
+		var $fromInput = $('.js-from-input'); //出发地
+		var $toInput = $('.js-to-input'); //目的地
+		var $fromMenuSub = $('.js-from-menu>li'); //出发地下拉菜单
+		var $toMenuSub = $('.js-to-menu>li'); //目的地下拉菜单
+
+		var $fromBox = $('.js-airport-from'); //出发地外层
+		var $toBox = $('.js-airport-to'); //目的地外层
+		var $dateBox = $('.js-popup-date'); //日期外层
+		var $peopleBox = $('.js-popup-people'); //人数选择外层
+
+		// c3动画
+		var popupShow = function(){
+			$content.removeClass('popup-inactive').addClass('popup-active'); 
+		}
+		var popupHide = function(){
+			$content.removeClass('popup-active').addClass('popup-inactive'); 
+		}
+
+		// 点击出发地
+		$fromInput.click(function(){
+			$mask.fadeIn(); //显示遮罩层
+			popupShow(); //增加c3动画
+			$box.addClass('popup-box-before');
+			$box.css('left',0);
+		});
+
+		// 点击目的地
+		$toInput.click(function(e){
+			$mask.fadeIn(); //显示遮罩层
+			popupShow(); //增加c3动画
+			$box.addClass('popup-box-before');
+			$box.css('left',350);
+		});
+
+		// 点击遮罩层
+		$mask.click(function(){
+			$(this).fadeOut();
+			popupHide(); //增加c3动画
+			$box.removeClass('popup-box-before'); //隐藏小箭头
+			$box.css('left',0);
+		});
+
+		// 出发地选择
+		$fromMenuSub.click(function(){
+			var text = $(this).html();
+			$fromInput.val(text);
+			$box.css('left',350);
+			$fromBox.slideUp(function(){ //出发地隐藏
+				$toBox.slideDown(); //目的地显示
+			}); 
+		});
+
+		// 目的地选择
+		$toMenuSub.click(function(){
+			var text = $(this).html();
+			$toInput.val(text);
+			$box.css('left',700);
+			$toBox.slideUp(function(){ //出发地隐藏
+				$('.js-date-result').click(); //日期展示
+				$dateBox.slideDown(); //目的地显示
+			}); 
+		});
+
+		/* 日期选择--动态加载 */
 		var formatDate = function(num) { //日期格式化
 			return num < 10 && (num = '0' + num);
 		};
@@ -58,15 +125,23 @@ var LanmeiAirlines = {
 				}else{
 					$('.js-date-result').html(start.format('MMM D') + ' - ' + end.format('MMM D'));
 				}
+
+				// 操作外层box移动
+				$box.css('left',950);
+				$dateBox.slideUp(function(){ //出发地隐藏
+					$peopleBox.slideDown(); //目的地显示
+				}); 
 		  } 
 		);
 	},
 
 	/* 人数选择 */
 	selectPeople:function(){
-		// 增加人数
 		var adultNum = 1;
 		var childNum = 0;
+		var infantNum = 0;
+
+		// 成人
 		var adult = function(){
 			$('.js-adult-add').click(function(){
 				adultNum++;
@@ -82,10 +157,73 @@ var LanmeiAirlines = {
 				$(this).siblings('span').html(adultNum);
 			});
 		}
+
+		// 小孩
+		var child = function(){
+			$('.js-child-add').click(function(){
+				childNum++;
+				$(this).siblings('span').html(childNum);
+				if(childNum==1){
+					$(this).siblings('.sub-people').removeClass('off-sub-operation');
+					$(this).parent().removeClass('disable');
+				}
+			});
+			$('.js-child-sub').click(function(){
+				childNum--;
+				if(childNum<1){
+					childNum=0;
+					$(this).addClass('off-sub-operation');
+					$(this).parent().addClass('disable');
+				}
+				$(this).siblings('span').html(childNum);
+			});
+		}
+
+		// 婴儿
+		var infant = function(){
+			$('.js-infant-add').click(function(){
+				infantNum++;
+				$(this).siblings('span').html(infantNum);
+				if(infantNum==1){
+					$(this).siblings('.sub-people').removeClass('off-sub-operation');
+					$(this).parent().removeClass('disable');
+				}
+			});
+			$('.js-infant-sub').click(function(){
+				infantNum--;
+				if(infantNum<1){
+					infantNum=0;
+					$(this).addClass('off-sub-operation');
+					$(this).parent().addClass('disable');
+				}
+				$(this).siblings('span').html(infantNum);
+			});
+		}
 		adult();
+		child();
+		infant();
+
+		/* 人数提示 */
+		var showAdultTip = 0;
+		var showChildTip = 0;
+		var showInfantTip = 0;
+		var tipFn = function(className,content,showTip){
+			$(className).mouseenter(function(event) {
+					showTip = layer.tips(content, className,{
+						tips: [2, '#8ec060'],
+						time: 0
+					});
+			}).mouseleave(function(event) {
+					layer.close(showTip);
+			});
+		}
+		tipFn('.adult-tip','Adult',showAdultTip);
+		tipFn('.child-tip','Passengers who have not reached their 12th birthday by the date of the last flight are considered child passengers Children 7 years old and older can travel alone with the consent of their parents.',showChildTip);
+		tipFn('.infant-tip','Passengers 7 days old up to those who have not reached their 2nd birthday travel with infant status.',showInfantTip);
+		
 	},
 
-	/* 后期优化新增 */
+	/* 其他事件 */
 	otherEvent:function(){
 		// 首屏自适应高度
 		var winHeight = $(window).height();
