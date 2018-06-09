5a7d6133a4a6bbbd828a9262514e07346ecdc7c8
liaozq009 <245822113@qq.com>
2018-06-09 11:21:26 +0800

    柬埔寨生活

--- webNavigation/js/lm-search.js
+++ webNavigation/js/lm-search.js
@@ -48,7 +48,7 @@ var LanmeiAirlines = {
 	otherEvent:function(){
 		var winWidth = $(window).width();
 		if(winWidth>992){
-			$('.js-hotSearch-box li:nth-child(10n+1)').css('margin-left','0');
+			$('.js-hotSearch-box li:nth-child(12n+1)').css('margin-left','0');
 			$('.js-searchList-wrap .searchList-box:nth-child(3n+2)').addClass('searchList-center');
 		}
 	},
