5e14cc48b00a4be6a03db8c55780f9735b09d39b
liaozq009 <245822113@qq.com>
2019-04-29 17:54:25 +0800

    动画demo

--- common/EN/js/common-data.js
+++ common/EN/js/common-data.js
@@ -1,5 +1,5 @@
 var LMComData = {
-    cityData: ['Bangkok/BKK/Thailand','Changsha/CSX/China','Chengdu/CTU/China','Guangzhou/CAN/China','Hanoi/HAN/Vietnam','HongKong/HKG/HongKong,China','Ho Chi Minh/SGN/Vietnam','Macao/MFM/Macao,China','Nanning/NNG/China','Phnom Penh/PNH/Cambodia','Shijiazhuang/SJW/China','Xi`an/XIY/China','Siem Reap/REP/Cambodia','Sihanoukville/KOS/Cambodia','Singapore/SIN/Singapore'],
+    cityData: ['Bangkok/BKK/Thailand','Changsha/CSX/China','Chengdu/CTU/China','Guangzhou/CAN/China','HongKong/HKG/HongKong,China','Ho Chi Minh/SGN/Vietnam','Macao/MFM/Macao,China','Nanning/NNG/China','Phnom Penh/PNH/Cambodia','Shijiazhuang/SJW/China','Xi`an/XIY/China','Siem Reap/REP/Cambodia','Sihanoukville/KOS/Cambodia'],
     fNumberData: ['LQ315','LQ316','LQ317','LQ318','LQ332','LQ333','LQ502','LQ503','LQ660','LQ661','LQ666','LQ667','LQ670','LQ671','LQ780','LQ781','LQ806','LQ807','LQ902','LQ903','LQ908','LQ909','LQ910','LQ911','LQ916','LQ917','LQ920','LQ921','LQ970','LQ971','LQ9302','LQ9303','LQ9508','LQ9509'],
   	cityDataAuto:{
   		'Phnom Penh/PNH/Cambodia': ['Guangzhou/CAN/China','HongKong/HKG/HongKong,China','Siem Reap/REP/Cambodia','Sihanoukville/KOS/Cambodia','Bangkok/BKK/Thailand'],
