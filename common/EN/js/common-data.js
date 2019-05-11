d8c2c847920d218d0c211f857475195f38fd1792
liaozq009 <245822113@qq.com>
2019-05-11 15:11:27 +0800

    退改签

--- common/EN/js/common-data.js
+++ common/EN/js/common-data.js
@@ -1,17 +1,19 @@
 var LMComData = {
-    cityData: ['Bangkok/BKK/Thailand','Changsha/CSX/China','Chengdu/CTU/China','Guangzhou/CAN/China','HongKong/HKG/HongKong,China','Ho Chi Minh/SGN/Vietnam','Macao/MFM/Macao,China','Nanning/NNG/China','Phnom Penh/PNH/Cambodia','Shijiazhuang/SJW/China','Xi`an/XIY/China','Siem Reap/REP/Cambodia','Sihanoukville/KOS/Cambodia'],
+    cityData: ['Bangkok/BKK/Thailand','Changsha/CSX/China','Chengdu/CTU/China','Guangzhou/CAN/China','HongKong/HKG/HongKong,China','Macao/MFM/Macao,China','Nanning/NNG/China','Nanjing/NKG/China','Phnom Penh/PNH/Cambodia','Shijiazhuang/SJW/China','Xi`an/XIY/China','Siem Reap/REP/Cambodia','Sihanoukville/KOS/Cambodia','Zhengzhou/CGO/China'],
     fNumberData: ['LQ315','LQ316','LQ317','LQ318','LQ332','LQ333','LQ502','LQ503','LQ660','LQ661','LQ666','LQ667','LQ670','LQ671','LQ780','LQ781','LQ806','LQ807','LQ902','LQ903','LQ908','LQ909','LQ910','LQ911','LQ916','LQ917','LQ920','LQ921','LQ970','LQ971','LQ9302','LQ9303','LQ9508','LQ9509'],
   	cityDataAuto:{
   		'Phnom Penh/PNH/Cambodia': ['Guangzhou/CAN/China','HongKong/HKG/HongKong,China','Siem Reap/REP/Cambodia','Sihanoukville/KOS/Cambodia','Bangkok/BKK/Thailand'],
   		'Siem Reap/REP/Cambodia': ['Phnom Penh/PNH/Cambodia','Bangkok/BKK/Thailand','Macao/MFM/Macao,China','Sihanoukville/KOS/Cambodia','Nanjing/NKG/China','Tianjin/TSN/China','Chengdu/CTU/China','Xi`an/XIY/China','Guangzhou/CAN/China'],
-  		'Sihanoukville/KOS/Cambodia': ['Phnom Penh/PNH/Cambodia','Tianjin/TSN/China','Macao/MFM/Macao,China','Guangzhou/CAN/China','Hong Kong/HKG/HongKong,China','Siem Reap/REP/Cambodia'],
+  		'Sihanoukville/KOS/Cambodia': ['Phnom Penh/PNH/Cambodia','Tianjin/TSN/China','Macao/MFM/Macao,China','Guangzhou/CAN/China','HongKong/HKG/HongKong,China','Siem Reap/REP/Cambodia','Zhengzhou/CGO/China','Changsha/CSX/China'],
   		'Bangkok/BKK/Thailand': ['Phnom Penh/PNH/Cambodia','Siem Reap/REP/Cambodia'],
-  		'Hong Kong/HKG/HongKong,China': ['Phnom Penh/PNH/Cambodia','Sihanoukville/KOS/Cambodia'],
+  		'HongKong/HKG/HongKong,China': ['Phnom Penh/PNH/Cambodia','Sihanoukville/KOS/Cambodia'],
   		'Guangzhou/CAN/China': ['Phnom Penh/PNH/Cambodia','Siem Reap/REP/Cambodia','Sihanoukville/KOS/Cambodia'],
   		'Macao/MFM/Macao,China': ['Siem Reap/REP/Cambodia','Sihanoukville/KOS/Cambodia'],
   		'Tianjin/TSN/China': ['Siem Reap/REP/Cambodia','Sihanoukville/KOS/Cambodia'],
   		'Nanjing/NKG/China': ['Siem Reap/REP/Cambodia'],
   		'Chengdu/CTU/China': ['Siem Reap/REP/Cambodia'],
   		'Xi`an/XIY/China': ['Siem Reap/REP/Cambodia'],
+      'Changsha/CSX/China': ['Sihanoukville/KOS/Cambodia'],
+      'Zhengzhou/CGO/China': ['Sihanoukville/KOS/Cambodia'],
   	}  
 };
